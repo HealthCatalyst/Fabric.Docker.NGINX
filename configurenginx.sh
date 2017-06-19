5b78190a394c5dade3f466b65c666da8b225ce48
michael.vidal <michael.vidal@healthcatalyst.com>
2017-06-19 19:53:19 -0400

    Updated the nginx image to use certificates from docker secrets if present

--- configurenginx.sh
+++ configurenginx.sh
@@ -2,17 +2,13 @@
 echo "configuring nginx..."
 host=$1
 remoteport=$2
-cat > /etc/nginx/conf.d/default.conf << EOF
-upstream dotnet {
-	server $host:$remoteport;
-}
 
-server {
+certificate=$3
+certificate_key=$4
 
-	listen 80;
-	location / {
-		proxy_pass http://dotnet;
-		proxy_http_version 1.1;
+locationdef="location / {
+                proxy_pass http://dotnet;
+                proxy_http_version 1.1;
 		proxy_set_header Upgrade \$http_upgrade;
 		proxy_set_header Connection keep-alive;
 		proxy_set_header Host \$host;
@@ -21,7 +17,28 @@ server {
 		proxy_set_header X-Forwarded-Proto \$scheme;
 		proxy_set_header X-Forwarded-Host \$server_name;
 		proxy_cache_bypass \$http_upgrade;
-	}
+       }"
+
+if ! [ -z "$certificate"  ] && ! [ -z "$certificate_key" ]; then
+    echo "certificates configured, setting up server on port 443"
+    listendef="listen 443 ssl http2 default_server;
+        listen [::]:443 ssl http2 default_server;
+
+	ssl_certificate /run/secrets/$certificate;
+	ssl_certificate_key /run/secrets/$certificate_key;"
+else
+    echo "no certificates configured, setting up server on port 80"
+    listendef="listen 80;"
+fi
+cat > /etc/nginx/conf.d/default.conf << EOF
+upstream dotnet {
+	server $host:$remoteport;
+}
+
+server {
+
+	$listendef
+	$locationdef
 }
 EOF
 echo "nginx configured as:"
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-./opt/install/configurenginx.sh $HOST $REMOTEPORT
+
+echo "cert path: /run/secrets/$CERTIFICATE"
+echo "cert_key path: /run/secrets/$CERTIFICATE_KEY"
+
+if ! [ -z "$CERTIFICATE" ] && ! [ -z "$CERTIFICATE_KEY" ]; then
+	./opt/install/configurenginx.sh $HOST $REMOTEPORT $CERTIFICATE $CERTIFICATE_KEY
+else
+	./opt/install/configurenginx.sh $HOST $REMOTEPORT
+fi
+
 exec nginx -g "daemon off;"
